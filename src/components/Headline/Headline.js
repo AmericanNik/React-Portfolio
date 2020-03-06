6001b8a7b85d5dfd6a7dd3f176b9cf69e118cc37
americannik <49841405+AmericanNik@users.noreply.github.com>
2020-03-06 16:03:49 -0500

    Scroll on clicks updated and color animation transitions

--- src/components/Headline/Headline.js
+++ src/components/Headline/Headline.js
@@ -2,13 +2,27 @@ import React from 'react';
 import './Headline.css';
 
 const Headline = () => {
+  const colors = ['red', '#f4f4f4', '#0077B5'];
+  let i = 0;
+
+  setInterval(() => {
+    let color = colors[(i = (i + 1) % colors.length)];
+    document.getElementById('headlineHighlight').style.color = color;
+    console.log(color);
+  }, 2000);
+
   return (
     <div>
       <div className='topOfPage' id='topOfPage'></div>
       <div id='Headline'>
         <div className='headlineSection' id='Home'>
           <div className='headlineContainer'>
-            <div>American Web Developer At Large</div>
+            <div>
+              <span className='headlineHighlight' id='headlineHighlight'>
+                American
+              </span>{' '}
+              <span className='remaining'> Web Developer At Large</span>
+            </div>
             <div className='iconContainer container'>
               <a target='_blank' href='https://www.linkedin.com/in/NikArboleda'>
                 <i class='fab fa-linkedin fa-2x linkedInIcon'></i>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -34,29 +34,29 @@ const Navbar = () => {
 
     if (window.innerWidth >= 1637) {
       console.log('Flag 1');
-      document.body.scrollTop = 0; // For Safari
+      document.body.scrollTop = 650; // For Safari
       document.documentElement.scrollTop = 650; // For Chrome, Firefox, IE and Opera
     }
     if (window.innerWidth < 1637 && window.innerWidth >= 921) {
       console.log('flag 2');
-      document.body.scrollTop = 550; // For Safari
+      document.body.scrollTop = 580; // For Safari
       document.documentElement.scrollTop = 580; // For Chrome, Firefox, IE and Opera
     }
 
     if (window.innerWidth <= 920 && window.innerWidth >= 751) {
       console.log('flag 3');
-      document.body.scrollTop = 550; // For Safari
+      document.body.scrollTop = 730; // For Safari
       document.documentElement.scrollTop = 730; // For Chrome, Firefox, IE and Opera
     }
 
     if (window.innerWidth < 750 && window.innerWidth >= 561) {
       console.log('flag 4');
-      document.body.scrollTop = 550; // For Safari
+      document.body.scrollTop = 400; // For Safari
       document.documentElement.scrollTop = 400; // For Chrome, Firefox, IE and
     }
     if (window.innerWidth <= 560) {
       console.log('flag 5');
-      document.body.scrollTop = 550; // For Safari
+      document.body.scrollTop = 500; // For Safari
       document.documentElement.scrollTop = 500; // For Chrome, Firefox, IE and
     }
   };
@@ -66,40 +66,40 @@ const Navbar = () => {
 
     if (window.innerWidth <= 518) {
       console.log('Fire 00');
-      document.body.scrollTop = 905; // For Safari
+      document.body.scrollTop = 1122; // For Safari
       document.documentElement.scrollTop = 1122; // For Chrome, Firefox, IE and Opera
     }
 
     if (window.innerWidth <= 750 && window.innerWidth >= 519) {
       console.log('Fire 0');
-      document.body.scrollTop = 905; // For Safari
+      document.body.scrollTop = 999; // For Safari
       document.documentElement.scrollTop = 999; // For Chrome, Firefox, IE and Opera
     }
 
     if (window.innerWidth <= 767 && window.innerWidth >= 751) {
       console.log('Fire1');
-      document.body.scrollTop = 905; // For Safari
+      document.body.scrollTop = 1130; // For Safari
       document.documentElement.scrollTop = 1330; // For Chrome, Firefox, IE and Opera
     }
 
     if (window.innerWidth <= 770 && window.innerWidth >= 768) {
       console.log('Fire2');
       console.log(window.innerWidth);
-      document.body.scrollTop = 800; // For Safari
+      document.body.scrollTop = 1256; // For Safari
       document.documentElement.scrollTop = 1256; // For Chrome, Firefox, IE and Opera
     }
 
     if (window.innerWidth <= 920 && window.innerWidth >= 771) {
       console.log('Fire3');
       console.log(window.innerWidth);
-      document.body.scrollTop = 920; // For Safari
+      document.body.scrollTop = 1128; // For Safari
       document.documentElement.scrollTop = 1128; // For Chrome, Firefox, IE and Opera
     }
 
     if (window.innerWidth >= 921 && window.innerWidth <= 991) {
       console.log('Fire4');
       console.log(window.innerWidth);
-      document.body.scrollTop = 920; // For Safari
+      document.body.scrollTop = 975; // For Safari
       document.documentElement.scrollTop = 975; // For Chrome, Firefox, IE and Opera
     }
 
@@ -112,15 +112,141 @@ const Navbar = () => {
     if (window.innerWidth > 1200 && window.innerWidth <= 1636) {
       console.log('Fire6');
       console.log(window.innerWidth);
-      document.body.scrollTop = 950; // For Safari
+      document.body.scrollTop = 883; // For Safari
       document.documentElement.scrollTop = 883; // For Chrome, Firefox, IE and Opera
     }
 
     if (window.innerWidth >= 1637) {
       console.log('Fire7');
       console.log(window.innerWidth);
-      document.body.scrollTop = 950; // For Safari
+      document.body.scrollTop = 949; // For Safari
       document.documentElement.scrollTop = 949; // For Chrome, Firefox, IE and Opera
+    }
+  };
+
+  const keeperFunction = () => {
+    console.log('Firing Window Length: ' + window.innerWidth);
+
+    if (window.innerWidth <= 518) {
+      console.log('Fire 00');
+      document.body.scrollTop = 1440; // For Safari
+      document.documentElement.scrollTop = 1440; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth <= 750 && window.innerWidth >= 519) {
+      console.log('Fire 0');
+      document.body.scrollTop = 1350; // For Safari
+      document.documentElement.scrollTop = 1350; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth <= 767 && window.innerWidth >= 751) {
+      console.log('Fire1');
+      document.body.scrollTop = 1680; // For Safari
+      document.documentElement.scrollTop = 1680; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth <= 770 && window.innerWidth >= 768) {
+      console.log('Fire2');
+      console.log(window.innerWidth);
+      document.body.scrollTop = 1708; // For Safari
+      document.documentElement.scrollTop = 1708; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth <= 920 && window.innerWidth >= 771) {
+      console.log('Fire3');
+      console.log(window.innerWidth);
+      document.body.scrollTop = 1578; // For Safari
+      document.documentElement.scrollTop = 1578; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth >= 921 && window.innerWidth <= 991) {
+      console.log('Fire4');
+      console.log(window.innerWidth);
+      document.body.scrollTop = 1428; // For Safari
+      document.documentElement.scrollTop = 1428; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth >= 992 && window.innerWidth <= 1200) {
+      console.log('Fire5');
+      console.log(window.innerWidth);
+      document.body.scrollTop = 1491; // For Safari
+      document.documentElement.scrollTop = 1491; // For Chrome, Firefox, IE and Opera
+    }
+    if (window.innerWidth > 1200 && window.innerWidth <= 1636) {
+      console.log('Fire6');
+      console.log(window.innerWidth);
+      document.body.scrollTop = 1570; // For Safari
+      document.documentElement.scrollTop = 1570; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth >= 1637) {
+      console.log('Fire7');
+      console.log(window.innerWidth);
+      document.body.scrollTop = 1640; // For Safari
+      document.documentElement.scrollTop = 1640; // For Chrome, Firefox, IE and Opera
+    }
+  };
+
+  const contactFunction = () => {
+    console.log('Firing Window Length: ' + window.innerWidth);
+
+    if (window.innerWidth <= 518) {
+      console.log('Fire 00');
+      document.body.scrollTop = 1985; // For Safari
+      document.documentElement.scrollTop = 1985; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth <= 750 && window.innerWidth >= 519) {
+      console.log('Fire 0');
+      document.body.scrollTop = 1911; // For Safari
+      document.documentElement.scrollTop = 1911; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth <= 767 && window.innerWidth >= 751) {
+      console.log('Fire1');
+      document.body.scrollTop = 2241; // For Safari
+      document.documentElement.scrollTop = 2241; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth <= 770 && window.innerWidth >= 768) {
+      console.log('Fire2');
+      console.log(window.innerWidth);
+      document.body.scrollTop = 2370; // For Safari
+      document.documentElement.scrollTop = 2370; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth <= 920 && window.innerWidth >= 771) {
+      console.log('Fire3');
+      console.log(window.innerWidth);
+      document.body.scrollTop = 2242; // For Safari
+      document.documentElement.scrollTop = 2242; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth >= 921 && window.innerWidth <= 991) {
+      console.log('Fire4');
+      console.log(window.innerWidth);
+      document.body.scrollTop = 2090; // For Safari
+      document.documentElement.scrollTop = 2090; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth >= 992 && window.innerWidth <= 1200) {
+      console.log('Fire5');
+      console.log(window.innerWidth);
+      document.body.scrollTop = 2300; // For Safari
+      document.documentElement.scrollTop = 2300; // For Chrome, Firefox, IE and Opera
+    }
+    if (window.innerWidth > 1200 && window.innerWidth <= 1636) {
+      console.log('Fire6');
+      console.log(window.innerWidth);
+      document.body.scrollTop = 2500; // For Safari
+      document.documentElement.scrollTop = 2500; // For Chrome, Firefox, IE and Opera
+    }
+
+    if (window.innerWidth >= 1637) {
+      console.log('Fire7');
+      console.log(window.innerWidth);
+      document.body.scrollTop = 2572; // For Safari
+      document.documentElement.scrollTop = 2572; // For Chrome, Firefox, IE and Opera
     }
   };
 
@@ -143,10 +269,10 @@ const Navbar = () => {
             <a href='#!' onClick={portfolioFunction}>
               <li>Portfolio</li>
             </a>
-            <a href='#Keeper'>
+            <a href='#!' onClick={keeperFunction}>
               <li>Keeper</li>
             </a>
-            <a href='#Contact'>
+            <a href='#!' onClick={contactFunction}>
               <li>Contact</li>
             </a>
           </ul>
